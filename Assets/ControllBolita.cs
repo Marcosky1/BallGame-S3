14552551c14469b45cda901949c4e18d5cafe716
imagozxd <144135582+imagozxd@users.noreply.github.com>
2024-09-05 12:05:48 -0500

    zz
    zz

---
+++ Assets/ControllBolita.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllBolita : MonoBehaviour
+{
+    public Material material;
+    public string colorProperty = "_color";
+    public float colorDuration = 1f;
+}
